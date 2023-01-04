8ae056b4635008ff7c47987f8264a42e4272f065
LeptusHe <heweidong526@gmail.com>
2023-01-04 13:47:45 +0800

    add different weight for vertex normal

--- apps/geom-prop-vis/main.cc
+++ apps/geom-prop-vis/main.cc
@@ -6,6 +6,8 @@
 #include <igl/opengl/glfw/Viewer.h>
 #include <fmt/format.h>
 
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
 Eigen::MatrixXd N_vertices;
 Eigen::MatrixXd N_faces;
 Eigen::MatrixXd N_corners;
@@ -18,9 +20,18 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
             viewer.data().set_normals(N_faces);
             return true;
         case '2':
+            igl::per_vertex_normals(V, F, igl::PER_VERTEX_NORMALS_WEIGHTING_TYPE_UNIFORM, N_vertices);
             viewer.data().set_normals(N_vertices);
             return true;
         case '3':
+            igl::per_vertex_normals(V, F, igl::PER_VERTEX_NORMALS_WEIGHTING_TYPE_AREA, N_vertices);
+            viewer.data().set_normals(N_vertices);
+            return true;
+        case '4':
+            igl::per_vertex_normals(V, F, igl::PER_VERTEX_NORMALS_WEIGHTING_TYPE_ANGLE, N_vertices);
+            viewer.data().set_normals(N_vertices);
+            return true;
+        case '5':
             viewer.data().set_normals(N_corners);
             return true;
         default: break;
@@ -30,9 +41,6 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 
 int main(int argc, char *argv[])
 {
-    Eigen::MatrixXd V;
-    Eigen::MatrixXi F;
-
     auto path = argc > 1 ? argv[1] : "data/bunny.off";
     if (!igl::read_triangle_mesh(path, V, F)) {
         std::cout << fmt::format("failed to load mesh: [{0}]", path);
@@ -43,12 +51,14 @@ int main(int argc, char *argv[])
     igl::per_face_normals(V, F, N_faces);
 
     // Compute per-vertex normals
-    igl::per_vertex_normals(V, F, N_vertices);
+    igl::per_vertex_normals(V, F,  N_vertices);
 
     // Compute per-corner normals, |dihedral angle| > 20 degrees --> crease
     igl::per_corner_normals(V, F, 20, N_corners);
 
     igl::opengl::glfw::Viewer viewer;
+    viewer.data().shininess
+
     viewer.data().set_mesh(V, F);
     viewer.callback_key_down = &key_down;
     viewer.data().set_normals(N_faces);
