07dc97e646c8c446bfb5f7a43476f925d50b8fb5
LeptusHe <heweidong526@gmail.com>
2023-01-04 13:38:09 +0800

    add per face, per vertex normal

--- apps/geom-prop-vis/main.cc
+++ apps/geom-prop-vis/main.cc
@@ -1,6 +1,32 @@
 #include <iostream>
 #include <igl/read_triangle_mesh.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/per_face_normals.h>
+#include <igl/per_corner_normals.h>
 #include <igl/opengl/glfw/Viewer.h>
+#include <fmt/format.h>
+
+Eigen::MatrixXd N_vertices;
+Eigen::MatrixXd N_faces;
+Eigen::MatrixXd N_corners;
+
+bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
+{
+    switch(key)
+    {
+        case '1':
+            viewer.data().set_normals(N_faces);
+            return true;
+        case '2':
+            viewer.data().set_normals(N_vertices);
+            return true;
+        case '3':
+            viewer.data().set_normals(N_corners);
+            return true;
+        default: break;
+    }
+    return false;
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,10 +35,25 @@ int main(int argc, char *argv[])
 
     auto path = argc > 1 ? argv[1] : "data/bunny.off";
     if (!igl::read_triangle_mesh(path, V, F)) {
-
+        std::cout << fmt::format("failed to load mesh: [{0}]", path);
+        return -1;
     }
 
+    // Compute per-face normals
+    igl::per_face_normals(V, F, N_faces);
 
+    // Compute per-vertex normals
+    igl::per_vertex_normals(V, F, N_vertices);
+
+    // Compute per-corner normals, |dihedral angle| > 20 degrees --> crease
+    igl::per_corner_normals(V, F, 20, N_corners);
+
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.callback_key_down = &key_down;
+    viewer.data().set_normals(N_faces);
+
+    viewer.launch();
 
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,20 @@
 add_rules("mode.debug", "mode.release")
-add_requires("libigl", {configs = {imgui=true}})
-add_requires(
-        "vcpkg::glad",
-        "vcpkg::eigen3",
-        "vcpkg::fmt")
+
+add_cxxflags("/bigobj")
+
+enable_debug = is_mode("debug")
+print("enable debug:", enable_debug)
+
+add_requires("libigl", {configs = {imgui=true}, debug=enable_debug})
+add_requires("fmt", {debug=enable_debug, header_only=true, system=false})
+add_requires("glad", {debug=enable_debug, system=false})
+add_requires("eigen", {debug=enable_debug, system=false})
 
 target("01-geometry-property-visualization")
     set_languages("c++17")
     set_kind("binary")
     add_files("apps/geom-prop-vis/*.cc")
-    add_packages("libigl", "vcpkg::glad", "vcpkg::eigen3", "vcpkg::fmt")
+    add_packages("libigl", "glad", "eigen", "fmt")
+    if (enable_debug) then
+        set_runtimes("MT")
+    end
