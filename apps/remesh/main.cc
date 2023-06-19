e5f0796f7d709d595b07579ec0b525b3c9b66b8d
LeptusHe <heweidong526@gmail.com>
2023-06-19 20:49:26 +0800

    refactor structures of source codes

--- apps/remesh/main.cc
+++ apps/remesh/main.cc
@@ -7,7 +7,7 @@
 #include <igl/opengl/glfw/imgui/ImGuiPlugin.h>
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
 #include <imgui.h>
-#include "mesh_utils.h"
+#include "utils/mesh_utils.h"
 #include <spdlog/spdlog.h>
 
 using Mesh = OpenMesh::TriMesh_ArrayKernelT<>;
