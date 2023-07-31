18cffc522a5d66420ce01a4bf3a1fd102727b5d0
LeptusHe <heweidong526@gmail.com>
2023-07-31 21:05:22 +0800

    实现顶点合并功能部分代码

--- src/utils/vertex_merger.cc
+++ src/utils/vertex_merger.cc
@@ -1,6 +1,18 @@
 #include "vertex_merger.h"
 
-void vertex_merger::Merge(Mesh& mesh, float distance_threshold)
+namespace {
+
+// https://github.com/g-truc/glm/blob/5c46b9c07008ae65cb81ab79cd677ecc1934b903/glm/gtx/hash.inl#L6
+void hash_combine(size_t& seed, size_t hash) const
+{
+    hash += 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^= hash;
+}
+
+}
+
+
+void vertex_merger::Merge(Mesh& mesh)
 {
     OpenMesh::VPropHandleT<bool> processed;
     mesh.add_property(processed, "processed");
@@ -28,4 +40,55 @@ void vertex_merger::Merge(Mesh& mesh, float distance_threshold)
 
     // 执行垃圾回收
     mesh.garbage_collection();
+}
+
+void vertex_merger::CollectMeshData(const Mesh& mesh)
+{
+    vertices.resize(mesh.n_vertices());
+    for (auto vertex : mesh.vertices()) {
+        auto idx = vertex.idx();
+        auto p = mesh.point(vertex);
+        vertices[idx] = p;
+    }
+
+    indices.resize(3 * mesh.n_faces());
+    for (auto fh : mesh.faces()) {
+        auto faceIndex = fh.idx();
+
+        int vIdx = 0;
+        for (auto vh : fh.vertices()) {
+            indices[3 * faceIndex + vIdx] = vh.idx();
+            vIdx += 1;
+        }
+    }
+}
+
+
+std::size_t vertex_merger::hash(const Mesh::Point& p) const
+{
+    auto x = static_cast<size_t>(p[0] * maxDigits);
+    auto y = static_cast<size_t>(p[1] * maxDigits);
+    auto z = static_cast<size_t>(p[2] * maxDigits);
+
+    size_t seed = 0;
+    std::hash<size_t> hasher;
+
+    hash_combine(seed, hasher(x));
+    hash_combine(seed, hasher(y));
+    hash_combine(seed, hasher(z));
+
+    return seed;
+}
+
+bool vertex_merger::equal(const Mesh::Point& lhs, const Mesh::Point& rhs) const
+{
+    auto lhs_x = static_cast<size_t>(lhs[0] * maxDigits);
+    auto lhs_y = static_cast<size_t>(lhs[1] * maxDigits);
+    auto lhs_z = static_cast<size_t>(lhs[2] * maxDigits);
+
+    auto rhs_x = static_cast<size_t>(rhs[0] * maxDigits);
+    auto rhs_y = static_cast<size_t>(rhs[1] * maxDigits);
+    auto rhs_z = static_cast<size_t>(rhs[2] * maxDigits);
+
+    return (lhs_x == rhs_x) && (lhs_y == rhs_y) && (lhs_z == rhs_z);
 }
--- src/utils/vertex_merger.h
+++ src/utils/vertex_merger.h
@@ -2,7 +2,19 @@
 #include "utils/mesh_io.h"
 
 class vertex_merger {
+private:
+    static constexpr float maxDigits = 1000000.0f;
+
 public:
     void Merge(Mesh& mesh);
-    void MergeVertex(Mesh::VertexHandle oldVertex, Mesh::VertexHandle newVertex)
+    void MergeVertex(Mesh::VertexHandle oldVertex, Mesh::VertexHandle newVertex);
+
+private:
+    void CollectMeshData(const Mesh& mesh);
+    [[nodiscard]] std::size_t hash(const Mesh::Point& p) const;
+    [[nodiscard]] bool equal(const Mesh::Point& lhs, const Mesh::Point& rhs) const;
+
+private:
+    std::vector<Mesh::Point> vertices;
+    std::vector<int> indices;
 };
